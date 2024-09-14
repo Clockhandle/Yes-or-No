4098bf5df42c3f19e74f806cd310234eca829e1c
Clockhandle <gamingiceman12@gmail.com>
2024-09-14 21:06:18 +0700

    Update LaptopController.cs

--- Assets/LaptopController.cs
+++ Assets/LaptopController.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using SUPERCharacter;
 using UnityEngine;
 
 public class LaptopController : MonoBehaviour, IInteractable
 {
     public SUPERCharacterAIO playerController;
+
+    public CinemachineVirtualCamera playerCamera;
+    public CinemachineVirtualCamera laptopViewCamera;
     public bool Interact()
     {
+<<<<<<< Updated upstream:Assets/LaptopController.cs
         Debug.Log("Activate Computer");
         playerController.interactRange = 0;
+=======
+        StartCoroutine(TransitioningStates());
+>>>>>>> Stashed changes:Assets/Scripts/LaptopController.cs
         return false;
+    }
+
+    IEnumerator TransitioningStates()
+    {
+        laptopViewCamera.Priority = 40;
+        yield return new WaitForSeconds(2);
+        Debug.Log("Activate Computer");
+        playerController.enabled = false;
     }
 }
